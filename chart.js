abee56c7364ce377b1b7ec617ece0685d051ae9b
luis0antonio55 <125217853+luis0antonio55@users.noreply.github.com>
2023-05-27 23:36:10 -0700

    chart color

--- chart.js
+++ chart.js
@@ -21,9 +21,8 @@ fetch('https://api.covidtracking.com/v1/us/daily.json')
         datasets: [{
           label: 'Muertes por COVID desde 2021',
           data: values,
-          backgroundColor: 'rgba(0, 123, 255, 0.5)', // Color de fondo de las barras
-          borderColor: 'rgba(0, 123, 255, 1)', // Color del borde de las barras
-          borderWidth: 1 // Ancho del borde de las barras
+          backgroundColor: 'rgb(68, 236, 213 )', // Color de fondo de las barras
+          borderWidth: 1 
         }]
       },
       options: {
